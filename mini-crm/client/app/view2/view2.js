a7443bbecfbfc4f6714ca321a8c55f2f26b8157b
Alex Woods <alex.woods89@hotmail.co.uk>
2019-12-17 14:55:08 +0000

    get request to front end

--- mini-crm/client/app/view2/view2.js
+++ mini-crm/client/app/view2/view2.js
@@ -5,7 +5,24 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: function candidateController(){
+      this.candidates = [
+        {name: "Alex"},
+        {name: "Bob"},
+        {name: "Tony"}
+      ]
+    }
+  
+  
+  //     ['$http', '$scope', function candidateController($http){
+  //     var self = this
+  //     $http.get('http://localhost:3000/api/candidates')
+  //     .then(function(response){
+  //       self.candidates = response.data
+  //       console.log(self.candidates)
+  //     })
+  //   }
+  // ]
   });
 }])
 
