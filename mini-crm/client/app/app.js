8db5eb2ebc57edf86fa8bf305d8642042d9a9f6c
Alex Woods <alex.woods89@hotmail.co.uk>
2019-12-20 12:21:56 +0000

    delete added

--- mini-crm/client/app/app.js
+++ mini-crm/client/app/app.js
@@ -5,34 +5,25 @@ myApp.controller('mainController', ['$scope', '$http', function ($scope, $http) 
     $scope.job = ''
     
     $scope.names = ["bob", "sarah"];
+    $scope.selectedCandidate = {}
     
     $scope.handle = '';
     
     $scope.pushToNames = function(){
         $scope.names.push($scope.handle)
+        $scope.handle = ''
     }
     
     $scope.candidates = []
 
-    $scope.addRule = function(){
+    $scope.addCandidate = function(){
       
         $http.post('http://localhost:3000/api/candidates', {name: $scope.name, job:$scope.job})
-        .then(function successCallback(response){
-          
-          $http({
-            method: 'GET',
-            url: 'http://localhost:3000/api/candidates'
-          }).then(function successCallback(response) {
-              $scope.candidates = response.data
-              
-            }, function errorCallback(response) {
-              console.error(error)
-            });
-            $scope.name = ''
-          $scope.job = ''
-        })
+        .then(get(),
+        $scope.name = '',
+            $scope.job = '')
 
-    }
+    }         
     
    $http({
   method: 'GET',
@@ -42,6 +33,22 @@ myApp.controller('mainController', ['$scope', '$http', function ($scope, $http) 
   }, function errorCallback(response) {
     console.error(error)
   });
+
+  var get = function(){ $http({
+    method: 'GET',
+    url: 'http://localhost:3000/api/candidates'
+  }).then(function successCallback(response) {
+      $scope.candidates = response.data
+    }, function errorCallback(response) {
+      console.error(error)
+    });
+  }
+
+  $scope.delete = function(){
+    $http.delete(`http://localhost:3000/api/candidates/${$scope.selectedCandidate._id}`)
+    .then(get())
+
+  }
 
    
     
